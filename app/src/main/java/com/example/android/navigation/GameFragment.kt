04b7e8a56e91b3205f42f9991ce5c68e11df5a05
jirawan.bn <jirawan.bn@rmuti.ac.th>
2020-07-28 17:42:13 +0700

    Android Kotlin Fundamentals 03.3_5_Step 2

--- app/src/main/java/com/example/android/navigation/GameFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameFragment.kt
@@ -102,7 +102,7 @@ class GameFragment : Fragment() {
                         // We've won!  Navigate to the gameWonFragment.
                         view.findNavController()
                                 .navigate(GameFragmentDirections
-                                        .actionGameFragmentToGameWonFragment(numQuestions, questionIndex)))
+                                        .actionGameFragmentToGameWonFragment(numQuestions, questionIndex))
                     }
                 } else {
                     // Game over! A wrong answer sends us to the gameOverFragment.
